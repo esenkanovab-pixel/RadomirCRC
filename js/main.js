6dc28ba2134d275a81e2f98124549826b6d2be5f
esenkanovab-pixel <esenkanov_ab@iuca.kg>
2025-11-17 09:58:27 +0600

    Fix: add slider existence check to prevent errors on pages without slider

--- js/main.js
+++ js/main.js
@@ -34,45 +34,47 @@ const next = document.querySelector('#next')
 const prev = document.querySelector('#prev')
 let index = 0
 
-const hideSlide = () => {
-    slides.forEach((slide) => {
-        slide.style.opacity = 0
-        slide.classList.remove('active_slide')
-    })
-}
-const showSlide = (i = 0) => {
-    slides[i].style.opacity = 1
-    slides[i].classList.add('active_slide')
-}
+// Только инициализируем слайдер если он существует
+if (slides.length > 0 && next && prev) {
+    const hideSlide = () => {
+        slides.forEach((slide) => {
+            slide.style.opacity = 0
+            slide.classList.remove('active_slide')
+        })
+    }
+    const showSlide = (i = 0) => {
+        slides[i].style.opacity = 1
+        slides[i].classList.add('active_slide')
+    }
 
-hideSlide()
-showSlide(index)
+    hideSlide()
+    showSlide(index)
 
+    const autoSlider = (i = 0) => {
+        setInterval(() => {
+            i++
+            if (i > slides.length - 1) {
+                i = 0
+            }
+            hideSlide()
+            showSlide(i)
+        }, 10000)
+    }
 
-const autoSlider = (i = 0) => {
-    setInterval(() => {
-        i++
-        if (i > slides.length - 1) {
-            i = 0
-        }
+    next.onclick = () => {
+        index < slides.length - 1 ? index++ : index = 0
         hideSlide()
-        showSlide(i)
-    }, 10000)
-}
+        showSlide(index)
+    }
 
-next.onclick = () => {
-    index < slides.length - 1 ? index++ : index = 0
-    hideSlide()
-    showSlide(index)
-}
+    prev.onclick = () => {
+        index > 0 ? index-- : index = slides.length - 1
+        hideSlide()
+        showSlide(index)
+    }
 
-prev.onclick = () => {
-    index > 0 ? index-- : index = slides.length - 1
-    hideSlide()
-    showSlide(index)
+    autoSlider(index)
 }
-
-autoSlider(index)
 
 // MODAL FUNCTIONALITY
 
